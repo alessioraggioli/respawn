bfb32712a78887b0c24f389fe3493bc8e40bc190
Alessio Lorenzo Raggioli <tsac047@tecnicosuperiorekennedy.it>
2018-03-10 20:47:58 +0100

    Update vue.js

--- js/vue.js
+++ js/vue.js
@@ -64,7 +64,7 @@ $(function () {
 
         $("body").css("background", "#f8f9fa");
         $("#app").addClass("animated fadeIn");
-        $("#app").load("../pages/catalog.html");
+        $("#app").load("https://alessioraggioli.github.io/respawn/pages/catalog.html");
         }, 750);
         
     });
